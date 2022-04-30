bbf9b5166c70bb2f394c5f728df09e66bd58281c
Laysa Diniz <95299576+santosdlaysa@users.noreply.github.com>
2022-04-30 18:34:24 -0300

    main

---
+++ desafio7dias/js/aulas/script.js
@@ -0,0 +1,90 @@
+/*
+//boolean - v ou f
+var vouf = false;
+console.log(typeof(vouf));
+ // number
+ var numeroqualquer = 1;
+ console.log(typeof(numeroqualquer));
+ //string
+ var nome = 'laysa';
+ console.log(typeof(nome));
+ //function
+ var funcao = function() {}
+ console.log(typeof(funcao)); */
+/*
+ //condição
+ var jogador1 = 0
+ var jogador2 = 0
+ var placar;
+
+ if (jogador1 > 0) {
+  console.log("Jogador 1 marcou ponto");
+ } else if (jogador2 > 0){
+   console.log("jogador 2 marcou ponto");
+ } else {
+   console.log("ninguem marcou ponto");
+ }
+
+switch (placar) {
+  case placar = jogador1 > jogador2:
+  console.log("jogador 1 granhou");
+  break;
+}
+*/
+/*
+let array = ['valor1', 'valor2', 'valor3', 'valor4','valor5'];
+
+let object = { propriedade1: 'valor1', propriedade2: 'valor2', propriedade3: 'valor3', }
+
+for (let indice = 0; indice < array.length; indice++) {
+  console.log(indice);
+}
+*/
+
+/*
+for (let i in array){
+  console.log(i);
+}
+*/
+//
+/*
+for (i in object) {
+  console.log(i);
+}
+*/
+/*
+// i of com array
+for (i of array){
+  console.log(i);
+}
+*/
+// com object
+/*
+for (i of object.propriedade1){
+  console.log(i);
+}
+*/
+/*
+//a++
+var a = 0;
+while(a < 10){
+  a++;
+  console.log(a);
+}
+
+do {
+  a++;
+  console.log(a);
+}while (a< 10)
+*/
+/*
+function funcao () {
+  console.log('tudo certo');
+}
+funcao ();
+*/
+
+function mensagem(primeiro, segundo) {
+  console.log(primeiro, segundo)
+}
+mensagem('tudo certo, jovem');
